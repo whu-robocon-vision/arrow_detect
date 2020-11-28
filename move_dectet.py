ca6efee40a95dab13464a5afd7d5ad3b3ccb8c4a
whxway <whxway@qq.com>
2020-11-28 07:05:06 -0500

    hh

--- move_dectet.py
+++ move_dectet.py
@@ -3,16 +3,20 @@ import cv2 as cv
 import sys
 import numpy as np
 
+
 def nothing(x):
     pass
+
 
 def getdistance(p1, p2):
     return np.int0(np.sqrt(np.square(p1[0] - p2[0]) + np.square(p1[1] - p2[1])))
 
-def get_extend_point(p1, p2, d1, d2, scale = 5):
-    px = np.int0((scale * d1 / d2 ) * (p1[0] - p2[0]) + p1[0])
-    py = np.int0((scale * d1 / d2 ) * (p1[1] - p2[1]) + p1[1])
+
+def get_extend_point(p1, p2, d1, d2, scale=5):
+    px = np.int0((scale * d1 / d2) * (p1[0] - p2[0]) + p1[0])
+    py = np.int0((scale * d1 / d2) * (p1[1] - p2[1]) + p1[1])
     return np.array([px, py])
+
 
 def get_extend_box(box):
     d1 = getdistance(box[0], box[1])
@@ -35,6 +39,7 @@ def get_extend_box(box):
         box2 = np.array([box[0], box[3], p2, p1])
         return box1, box2
 
+
 def check_arrow_head(frame, box):
     mask = np.zeros(frame.shape[:2], dtype=np.uint8)
     mask = cv.fillPoly(mask, [box], 255)
@@ -46,6 +51,7 @@ def check_arrow_head(frame, box):
     else:
         return False
 
+
 def check_head(box, frame):
     box1, box2 = get_extend_box(box)
     flag1 = check_arrow_head(frame, box1)
@@ -56,7 +62,8 @@ def check_head(box, frame):
         cv.drawContours(frame, [box2], 0, (255, 0, 255), 3)
     return flag1 or flag2
 
-def extrace_object_demo():
+
+def start_rs():
     pipeline = rs.pipeline()
     cfg = rs.config()
     cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
@@ -64,17 +71,18 @@ def extrace_object_demo():
     cfg.enable_stream(rs.stream.infrared, 1, 640, 480, rs.format.y8, 60)
     cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 60)
     profile = pipeline.start(cfg)
-    # capture = cv.VideoCapture(0)
-    
     depth_sensor = profile.get_device().first_depth_sensor()
     depth_scale = depth_sensor.get_depth_scale()
 
-    clipping_distance_in_meters = 1 #1 meter
+    clipping_distance_in_meters = 1  # 1 meter
     clipping_distance = clipping_distance_in_meters / depth_scale
 
     align_to = rs.stream.color
     align = rs.align(align_to)
+    return pipeline, align
 
+
+def create_window():
     cv.namedWindow("param")
     cv.resizeWindow("param", 400, 800)
     cv.createTrackbar('Rl', "param", 0, 255, nothing)
@@ -93,106 +101,59 @@ def extrace_object_demo():
     cv.createTrackbar('wgl', 'param', 110, 255, nothing)
     cv.createTrackbar('wbl', 'param', 110, 255, nothing)
 
-    while True:
-        frames = pipeline.wait_for_frames()
-        aligned_frames = align.process(frames)
 
-        color_frame = aligned_frames.get_color_frame()
-        if color_frame is None:
-            continue
-        color_frame = np.asanyarray(color_frame.get_data())
-        frame = color_frame
-        # frame = cv.GaussianBlur(color_frame, (7, 7), 0)
-        # cv.imshow("blurred", frame)
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(frame,
-            lowerb=np.array([cv.getTrackbarPos('Bl', 'param'), cv.getTrackbarPos('Gl', 'param'), cv.getTrackbarPos('Rl', 'param')]), 
-            upperb=np.array([cv.getTrackbarPos('Bh', 'param'), cv.getTrackbarPos('Gh', 'param'), cv.getTrackbarPos('Rh', 'param')]))
-        # mask = cv.bitwise_not(mask)
-        cv.imshow("range", mask)
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (cv.getTrackbarPos('kernel', 'param'), cv.getTrackbarPos('kernel', 'param')))
-        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=1)
+def detect_arrow(color_frame):
+    frame = cv.cvtColor(color_frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(frame,
+                        lowerb=np.array([cv.getTrackbarPos('Bl', 'param'), cv.getTrackbarPos(
+                            'Gl', 'param'), cv.getTrackbarPos('Rl', 'param')]),
+                        upperb=np.array([cv.getTrackbarPos('Bh', 'param'), cv.getTrackbarPos('Gh', 'param'), cv.getTrackbarPos('Rh', 'param')]))
+    # mask = cv.bitwise_not(mask)
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (cv.getTrackbarPos(
+        'kernel', 'param'), cv.getTrackbarPos('kernel', 'param')))
+    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=1)
+    # mask = cv.Laplacian(mask, cv.CV_8U, ksize=5)
+    _, contours, heriachy = cv.findContours(
+        mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for i, contour in enumerate(contours):
+        # epsilon = 0.1 * cv.arcLength(contour, True)
+        # contour = cv.approxPolyDP(contour, epsilon, True)
 
-        # mask = cv.Laplacian(mask, cv.CV_8U, ksize=5)
-        _, contours, heriachy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        for i, contour in enumerate(contours):
-
-            # epsilon = 0.1 * cv.arcLength(contour, True)
-            # contour = cv.approxPolyDP(contour, epsilon, True)
-
-            # contour = cv.convexHull(contour)
-
-            contour_area = cv.contourArea(contour)
-            rect = cv.minAreaRect(contour)
-            w, h = rect[1]
-            # cv.drawContours(frame, contours, i, (255, 0, 255), 3)
-            if min(w, h) == 0:
-                scale = 0
-            else:
-                scale = max(w, h) / min(w, h)
-            if  (contour_area > cv.getTrackbarPos('factor', 'param') / 10 * w * h 
-                and contour_area > cv.getTrackbarPos('area_min', 'param')
-                and contour_area < cv.getTrackbarPos('area_max', 'param')
+        # contour = cv.convexHull(contour)
+        contour_area = cv.contourArea(contour)
+        rect = cv.minAreaRect(contour)
+        w, h = rect[1]
+        # cv.drawContours(frame, contours, i, (255, 0, 255), 3)
+        if min(w, h) == 0:
+            scale = 0
+        else:
+            scale = max(w, h) / min(w, h)
+        if (contour_area > cv.getTrackbarPos('factor', 'param') / 10 * w * h
+            and contour_area > cv.getTrackbarPos('area_min', 'param')
+            and contour_area < cv.getTrackbarPos('area_max', 'param')
                 and scale > cv.getTrackbarPos('scale', 'param')):
-                
-                box = cv.boxPoints(rect)
-                box = np.int0(box)
-                if check_head(box, color_frame):
-                    cv.drawContours(color_frame, [box], 0, (0, 0, 255), 3)
-                    mm = cv.moments(contour)
-                    cx = mm['m10'] / mm['m00']
-                    cy = mm['m01'] / mm['m00']
-                    cv.circle(color_frame, (np.int(cx), np.int(cy)), 3, (0, 255, 255), -1)
-                
-        cv.imshow("mask", mask)
-        cv.imshow("frame", color_frame)
 
-        c = cv.waitKey(1)
-        if c == 27:
-            cv.destroyAllWindows()
-            break
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            if check_head(box, color_frame):
+                cv.drawContours(color_frame, [box], 0, (0, 0, 255), 3)
+                mm = cv.moments(contour)
+                cx = mm['m10'] / mm['m00']
+                cy = mm['m01'] / mm['m00']
+                cv.circle(color_frame, (np.int(cx), np.int(cy)),
+                            3, (0, 255, 255), -1)
+    cv.imshow("color_frame", color_frame)
 
 
-#     while(True):
-#         frames = pipeline.wait_for_frames()
-#         frame = frames.get_color_frame()
-
-
-#         # ret, frame = capture.read()
-#         # if ret == False:
-#         #     break
-
-#         # lower_hsv = np.array([156, 60, 46])
-#         # upper_hsv = np.array([180, 255, 255])
-#         # frame = cv.pyrMeanShiftFiltering(frame, 20, 100)
-#         frame = cv.GaussianBlur(frame, (3, 3), 0)
-#         # hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-#         # mask1 = cv.inRange(hsv, lowerb=lower_hsv, upperb=upper_hsv)
-#         mask = cv.inRange(frame, lowerb=np.array([0, 0, 110]), upperb=np.array([110, 110, 255]))
-#         kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
-#         mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-#         # mask = cv.bitwise_or(mask1, mask2)
-
-        
-#         # dst = cv.bitwise_and(frame, frame, mask=mask)
-
-#         contours, heriachy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-#         for i, contour in enumerate(contours):
-#             contour_area = cv.contourArea(contour)
-#             x, y, w, h = cv.boundingRect(contour)
-#             cv.drawContours(mask, contours, i, (0, 0, 255), 3)
-#             if contour_area > 0.8 * w * h:
-#                 cv.drawContours(frame, contours, i, (255, 0, 255), 2)
-
-#         cv.imshow("mask", mask)
-#         cv.imshow("video", frame)
-        
-#         if cv.waitKey(40) == 27:
-#             break
-
-t1 = cv.getTickCount()
-extrace_object_demo()
-t2 = cv.getTickCount()
-print("time: %s ms"%((t2 - t1) / cv.getTickFrequency() * 1000))
-
-# cv.destroyAllWindows()
+pipeline, align = start_rs()   
+create_window()
+while True:
+    frames = pipeline.wait_for_frames()
+    aligned_frames = align.process(frames)
+    color_frame = aligned_frames.get_color_frame()
+    if color_frame is None:
+        continue
+    color_frame = np.asanyarray(color_frame.get_data())
+    detect_arrow(color_frame)
+    if cv.waitKey(1) == 27:
+        break
